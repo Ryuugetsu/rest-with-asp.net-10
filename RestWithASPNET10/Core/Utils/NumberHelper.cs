81d0e09c07942a691f1a18713f9ec111b43202f5
Bruno Marinho (Home) <bruno.200926@hotmail.com>
2026-03-05 02:05:40 -0300

    V0.0.5 - Greeting and Math Endpoints - Get test

---
+++ RestWithASPNET10/Core/Utils/NumberHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class NumberHelper
+    {
+        public static decimal ConvertToDecimal(string value)
+        {
+            decimal.TryParse(value,
+                             System.Globalization.NumberStyles.Any,
+                             System.Globalization.NumberFormatInfo.InvariantInfo,
+                             out decimal number);
+            return number;
+        }
+
+        public static bool IsNumeric(string value)
+        {
+            return decimal.TryParse(value,
+                                    System.Globalization.NumberStyles.Any,
+                                    System.Globalization.NumberFormatInfo.InvariantInfo,
+                                    out decimal number);
+        }
+    }
+}
